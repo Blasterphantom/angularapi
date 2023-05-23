dea78b7ae83ac494a90f45be765f6187de8b4676
Blasterphantom <blasterneko@gmail.com>
2023-05-23 14:25:45 -0700

    Changed from digimon design to yugioh design api

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  constructor() { }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      const digimonData = await this.GetDigimonData();
+      console.log(digimonData);
+    } catch (error) {
+      console.error('Error fetching digimon data:', error);
+    }
+  }
+
+  async GetDigimonData(){
+    let res = await fetch(`/digi-api.com/api/v1/digimon/agumon`);
+    let data = await res.json();
+    let digiData = data;
+    console.log(digiData);
+    return data;
+  }
+  
 }
